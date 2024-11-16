6e9ec3b852135ff1cf83e5fca5fd8ac59b51dd86
Dereck Chen <92903435+derecknowayback@users.noreply.github.com>
2024-11-16 12:28:15 +0800

    feat(config): 更新导航栏和社会媒体链接
    - 在导航栏中添加分类链接
    - 移除无效的社交媒体链接
    - 新增 bilibili 和 zhihu 社交媒体图标
    - 更新 mastodon 链接
    - 启用标签云概述

--- source/js/main.js
+++ source/js/main.js
@@ -57,9 +57,9 @@ $(document).ready(function() {
     if (menu.length) {
       $(window).on("scroll", function() {
         var topDistance = menu.offset().top;
-
+        
         // hide only the navigation links on desktop
-        if (!nav.is(":visible") && topDistance < 50) {
+        if (!nav.is(":visible") && topDistance < 100) {
           nav.show();
         } else if (nav.is(":visible") && topDistance > 100) {
           nav.hide();
